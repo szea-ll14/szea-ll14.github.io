7341af3489c32da81956eacfb07f86d9116c2f7c
szea-ll14 <113614852+szea-ll14@users.noreply.github.com>
2025-03-23 17:08:52 +0900

    change

--- fmbeSim/main.js
+++ fmbeSim/main.js
@@ -1,74 +1,73 @@
-document.addEventListener('DOMContentLoaded', () => {
-  // HTMLからcanvas要素を取得する
-  const canvas = document.getElementById('canvas');
+// WebGLコンテキストを取得
+const canvas = document.getElementById('canvas');
+const gl = canvas.getContext('webgl');
 
-  // canvas要素からwebglコンテキストを取得する
-  const gl = canvas.getContext('webgl');
-  if (!gl) {
-    alert('webgl not supported!');
-    return;
-  }
+// canvasを初期化
+gl.clearColor(0.1, 0.1, 0.1, 1.0);
+gl.clear(gl.COLOR_BUFFER_BIT);
 
-  // canvasを初期化する
-  gl.clearColor(0.9, 0.9, 0.9, 1.0);
-  gl.clear(gl.COLOR_BUFFER_BIT);
+// シェーダーをコンパイル
+const vertSource = document.getElementById('vertShader').textContent;
+const vertShader = gl.createShader(gl.VERTEX_SHADER);
+gl.shaderSource(vertShader, vertSource);
+gl.compileShader(vertShader);
+const fragSource = document.getElementById('fragShader').textContent;
+const fragShader = gl.createShader(gl.FRAGMENT_SHADER);
+gl.shaderSource(fragShader, fragSource);
+gl.compileShader(fragShader);
 
-  // プログラムオブジェクトを作成する
-  const program = gl.createProgram();  
+// プログラムオブジェクトを作成
+const program = gl.createProgram();
 
-  // シェーダのソースを取得する
-  const vertexShaderSource = document.getElementById('vertexShader').textContent;
-  const fragmentShaderSource = document.getElementById('fragmentShader').textContent;
+// シェーダーをリンク
+gl.attachShader(program, vertShader);
+gl.deleteShader(vertShader);
+gl.attachShader(program, fragShader);
+gl.deleteShader(fragShader);
+gl.linkProgram(program);
 
-  // シェーダをコンパイルして、プログラムオブジェクトにシェーダを割り当てる
-  const vertexShader = gl.createShader(gl.VERTEX_SHADER);
-  gl.shaderSource(vertexShader, vertexShaderSource);
-  gl.compileShader(vertexShader);
-  gl.attachShader(program, vertexShader);
-  const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-  gl.shaderSource(fragmentShader, fragmentShaderSource);
-  gl.compileShader(fragmentShader);
-  gl.attachShader(program, fragmentShader);
+// プログラムオブジェクトを有効にする
+gl.useProgram(program);
 
-  // シェーダをリンクする
-  gl.linkProgram(program);
-  // プログラムオブジェクトを有効にする
-  gl.useProgram(program);
 
-  // 3つの頂点の座標を定義する
-  const triangleVertexPosition = [
-    0.0, 0.8, 0.0,
-    -0.8, -0.8, 0.0,
-    0.8, -0.8, 0.0
-  ];
+// 3つの頂点の座標を定義する
+const blockvertexPos = [
+  // above
+  // below
+  // left
+  // front
+  // right
+  // back
+  0.0, 0.8, 0.0,
+  -0.8, -0.8, 0.0,
+  0.8, -0.8, 0.0
+];
 
-  // 頂点バッファを作成する
-  const triangleVertexBuffer = gl.createBuffer();
-  // 頂点バッファをバインドする
-  gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBuffer);
-  // 頂点バッファに頂点データをセットする
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertexPosition), gl.STATIC_DRAW);
-  
-  // Positionのロケーションを取得し、バッファを割り当てる
-  const positionLocation = gl.getAttribLocation(program, 'position');
-  gl.enableVertexAttribArray(positionLocation);
-  gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
+// 頂点バッファを作成する
+const triangleVertexBuffer = gl.createBuffer();
+// 頂点バッファをバインドする
+gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBuffer);
+// 頂点バッファに頂点データをセットする
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(blockvertexPos), gl.STATIC_DRAW);
 
-  // 描画する
-  gl.drawArrays(gl.TRIANGLES, 0, 3);
-  gl.flush();
+// Positionのロケーションを取得し、バッファを割り当てる
+const positionLocation = gl.getAttribLocation(program, 'position');
+gl.enableVertexAttribArray(positionLocation);
+gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
 
-  const fmbeVarList = [
-    "xpos", "ypos", "zpos",
-    "xrot", "yrot", "zrot",
-    "scale", "xzscale", "yscale",
-    "xbasepos", "ybasepos", "zbasepos",
-  ]
+// 描画する
+gl.drawArrays(gl.TRIANGLES, 0, 3);
+gl.flush();
 
-  for (const fmbeVar of fmbeVarList) {
-    document.getElementById(fmbeVar)
-  }
+const fmbeVarList = [
+  "xpos", "ypos", "zpos",
+  "xrot", "yrot", "zrot",
+  "scale", "xzscale", "yscale",
+  "xbasepos", "ybasepos", "zbasepos",
+]
 
-  function reset(fmbeVar) {}
+for (const fmbeVar of fmbeVarList) {
+  document.getElementById(fmbeVar)
+}
 
-});
+function reset() {}
