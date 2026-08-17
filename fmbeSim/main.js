a925821614e62d14c476da2433aa7bd4a440092d
szea-ll14 <cezhe157@gmail.com>
2026-08-18 08:19:23 +0900

    adjMat改良

--- fmbeSim/main.js
+++ fmbeSim/main.js
@@ -20,23 +20,37 @@ function tMat(mat) {
 }
 // 余因子行列
 function adjMat(mat) {
+  const s0 = mat[ 0] * mat[ 5] - mat[ 1] * mat[ 4];
+  const s1 = mat[ 0] * mat[ 6] - mat[ 2] * mat[ 4];
+  const s2 = mat[ 0] * mat[ 7] - mat[ 3] * mat[ 4];
+  const s3 = mat[ 1] * mat[ 6] - mat[ 2] * mat[ 5];
+  const s4 = mat[ 1] * mat[ 7] - mat[ 3] * mat[ 5];
+  const s5 = mat[ 2] * mat[ 7] - mat[ 3] * mat[ 6];
+
+  const c0 = mat[ 8] * mat[13] - mat[ 9] * mat[12];
+  const c1 = mat[ 8] * mat[14] - mat[10] * mat[12];
+  const c2 = mat[ 8] * mat[15] - mat[11] * mat[12];
+  const c3 = mat[ 9] * mat[14] - mat[10] * mat[13];
+  const c4 = mat[ 9] * mat[15] - mat[11] * mat[13];
+  const c5 = mat[10] * mat[15] - mat[11] * mat[14];
+
   return [
-    mat[5]*(mat[10]*mat[15] - mat[11]*mat[14]) + mat[6]*(mat[11]*mat[13] - mat[ 9]*mat[15]) + mat[7]*(mat[ 9]*mat[14] - mat[10]*mat[13]),
-    mat[1]*(mat[11]*mat[14] - mat[10]*mat[15]) + mat[2]*(mat[ 9]*mat[15] - mat[11]*mat[13]) + mat[3]*(mat[10]*mat[13] - mat[ 9]*mat[14]),
-    mat[1]*(mat[ 6]*mat[15] - mat[ 7]*mat[14]) + mat[2]*(mat[ 7]*mat[13] - mat[ 5]*mat[15]) + mat[3]*(mat[ 5]*mat[14] - mat[ 6]*mat[13]),
-    mat[1]*(mat[ 7]*mat[10] - mat[ 6]*mat[11]) + mat[2]*(mat[ 5]*mat[11] - mat[ 7]*mat[ 9]) + mat[3]*(mat[ 6]*mat[ 9] - mat[ 5]*mat[10]),
-    mat[4]*(mat[11]*mat[14] - mat[10]*mat[15]) + mat[6]*(mat[ 8]*mat[15] - mat[11]*mat[12]) + mat[7]*(mat[10]*mat[12] - mat[ 8]*mat[14]),
-    mat[0]*(mat[10]*mat[15] - mat[11]*mat[14]) + mat[2]*(mat[11]*mat[12] - mat[ 8]*mat[15]) + mat[3]*(mat[ 8]*mat[14] - mat[10]*mat[12]),
-    mat[0]*(mat[ 7]*mat[14] - mat[ 6]*mat[15]) + mat[2]*(mat[ 4]*mat[15] - mat[ 7]*mat[12]) + mat[3]*(mat[ 6]*mat[12] - mat[ 4]*mat[14]),
-    mat[0]*(mat[ 6]*mat[11] - mat[ 7]*mat[10]) + mat[2]*(mat[ 7]*mat[ 8] - mat[ 4]*mat[11]) + mat[3]*(mat[ 4]*mat[10] - mat[ 6]*mat[ 8]),
-    mat[4]*(mat[ 9]*mat[15] - mat[11]*mat[13]) + mat[5]*(mat[11]*mat[12] - mat[ 8]*mat[15]) + mat[7]*(mat[ 8]*mat[13] - mat[ 9]*mat[12]),
-    mat[0]*(mat[11]*mat[13] - mat[ 9]*mat[15]) + mat[1]*(mat[ 8]*mat[15] - mat[11]*mat[12]) + mat[3]*(mat[ 9]*mat[12] - mat[ 8]*mat[13]),
-    mat[0]*(mat[ 5]*mat[15] - mat[ 7]*mat[13]) + mat[1]*(mat[ 7]*mat[12] - mat[ 4]*mat[15]) + mat[3]*(mat[ 4]*mat[13] - mat[ 5]*mat[12]),
-    mat[0]*(mat[ 7]*mat[ 9] - mat[ 5]*mat[11]) + mat[1]*(mat[ 4]*mat[11] - mat[ 7]*mat[ 8]) + mat[3]*(mat[ 5]*mat[ 8] - mat[ 4]*mat[ 9]),
-    mat[4]*(mat[10]*mat[13] - mat[ 9]*mat[14]) + mat[5]*(mat[ 8]*mat[14] - mat[10]*mat[12]) + mat[6]*(mat[ 9]*mat[12] - mat[ 8]*mat[13]),
-    mat[0]*(mat[ 9]*mat[14] - mat[10]*mat[13]) + mat[1]*(mat[10]*mat[12] - mat[ 8]*mat[14]) + mat[2]*(mat[ 8]*mat[13] - mat[ 9]*mat[12]),
-    mat[0]*(mat[ 6]*mat[13] - mat[ 5]*mat[14]) + mat[1]*(mat[ 4]*mat[14] - mat[ 6]*mat[12]) + mat[2]*(mat[ 5]*mat[12] - mat[ 4]*mat[13]),
-    mat[0]*(mat[ 5]*mat[10] - mat[ 6]*mat[ 9]) + mat[1]*(mat[ 6]*mat[ 8] - mat[ 4]*mat[10]) + mat[2]*(mat[ 4]*mat[ 9] - mat[ 5]*mat[ 8])
+    mat[ 5] * c5 - mat[ 6] * c4 + mat[ 7] * c3,
+  - mat[ 1] * c5 + mat[ 2] * c4 - mat[ 3] * c3,
+    mat[13] * s5 - mat[14] * s4 + mat[15] * s3,
+  - mat[ 9] * s5 + mat[10] * s4 - mat[11] * s3,
+  - mat[ 4] * c5 + mat[ 6] * c2 - mat[ 7] * c1,
+    mat[ 0] * c5 - mat[ 2] * c2 + mat[ 3] * c1,
+  - mat[12] * s5 + mat[14] * s2 - mat[15] * s1,
+  + mat[ 8] * s5 - mat[10] * s2 + mat[11] * s1,
+    mat[ 4] * c4 - mat[ 5] * c2 + mat[ 7] * c0,
+  - mat[ 0] * c4 + mat[ 1] * c2 - mat[ 3] * c0,
+  + mat[12] * s4 - mat[13] * s2 + mat[15] * s0,
+  - mat[ 8] * s4 + mat[ 9] * s2 - mat[11] * s0,
+  - mat[ 4] * c3 + mat[ 5] * c1 - mat[ 6] * c0,
+    mat[ 0] * c3 - mat[ 1] * c1 + mat[ 2] * c0,
+  - mat[12] * s3 + mat[13] * s1 - mat[14] * s0,
+  + mat[ 8] * s3 - mat[ 9] * s1 + mat[10] * s0
   ];
 }
 // 数値を文字列化: 指数表記ではなく整数・小数で
